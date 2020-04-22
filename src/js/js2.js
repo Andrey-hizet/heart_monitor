17e16a10294ae18bdb8ee3865e23743019621ff9
andrey <bachinskiy.1987@mail.ru>
2020-04-22 20:25:29 +0300

    add tabs

---
+++ src/js/js2.js
@@ -0,0 +1,50 @@
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         dots: true,
+//         speed: 1200,
+//         adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/prev.png"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="img/slider/next.png"></button>',
+//         responsive: [
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                 dots: true,
+//                 arrows: false
+//                 }
+//               }
+//         ]
+//       });
+      
+//   });
+
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
+  });
+
+  document.querySelector('.prev').addEventListener ('click', function () {
+    slider.goTo('prev');
+  });
+
+  document.querySelector('.next').addEventListener ('click', function () {
+    slider.goTo('next');
+  });
+
+  
+  (function($) {
+    $(function() {
+      
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+      });
+      
+    });
+    })(jQuery);
